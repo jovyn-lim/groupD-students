bdfa039639e1e11fb576c0a39899c4941e5c8186
Arul <arulkumaran0108@gmail.com>
2022-08-27 22:06:23 +0800

    Created Student class

--- src/main/java/com/pinelabs/groupDstudents/GroupDStudentsApplication.java
+++ src/main/java/com/pinelabs/groupDstudents/GroupDStudentsApplication.java
@@ -2,12 +2,19 @@ package com.pinelabs.groupDstudents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class GroupDStudentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GroupDStudentsApplication.class, args);
 	}
+
 
 }
