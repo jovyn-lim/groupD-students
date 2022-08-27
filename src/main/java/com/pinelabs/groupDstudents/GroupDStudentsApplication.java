e1b2aaf4fcb2cc09f321cc5137d58b2db99eb50f
Jovyn Lim <jovyn.lim@pinelabs.com>
2022-08-27 16:43:25 +0800

    Initial Commit

---
+++ src/main/java/com/pinelabs/groupDstudents/GroupDStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.pinelabs.groupDstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupDStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupDStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinelabs/groupDstudents/GroupDStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinelabs.groupDstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupDStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
