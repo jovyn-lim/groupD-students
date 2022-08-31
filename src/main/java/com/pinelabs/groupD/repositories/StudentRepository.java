83726b8d59011f5364c4d5ca01bae07e92fcae28
Jovyn Lim <jovyn.lim@pinelabs.com>
2022-08-31 23:29:27 +0800

    Minor code tidying

--- src/main/java/com/pinelabs/groupD/repositories/StudentRepository.java
+++ src/main/java/com/pinelabs/groupD/repositories/StudentRepository.java
@@ -2,7 +2,6 @@ package com.pinelabs.groupD.repositories;
 
 import com.pinelabs.groupD.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +10,5 @@ import java.util.Optional;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Optional<Student> findStudentById(Long studentId);
-    //@Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
